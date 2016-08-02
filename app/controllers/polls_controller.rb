6975d2933091ef9acea890536b1987bb79a6ef4b
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-08-02 17:34:55 -0600

    set model associations

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,6 +1,26 @@
 class PollsController < ApplicationController
+  before_action find_poll, only [:show, :delete, :update]
   def show
-    @poll = Poll.find(params[:id])
     render json: @poll
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+    render json: @poll
+  end
+
+  def delete
+    render json: @poll
+  end
+
+  private
+
+  def find_poll
+    @poll = Poll.find(params[:id])
   end
 end
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,27 @@
+class QuestionsController < ApplicationController
+  before_action find_question, only [:show, :delete, :update]
+
+  def show
+    render json: @question
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+    render json: @poll
+  end
+
+  def delete
+    render json: @poll
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+end
